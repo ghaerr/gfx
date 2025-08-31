041bbeb35d7aa70b8aaad04617b7a4312936aa09
Greg Haerr <greg@censoft.com>
2025-08-30 20:01:20 -0700

    Separate draw.h from draw.c

--- Makefile
+++ Makefile
@@ -4,7 +4,8 @@ CFLAGS = -Wall -Wno-missing-braces
 LDLIBS += -lSDL2
 
 # generated font files
-GENFONTSRCS = cour_32.c cour_32_tt.c times_32.c times_32_tt.c lucida_32.c lucida_32_tt.c
+GENFONTSRCS = cour_32.c cour_32_tt.c
+#GENFONTSRCS += times_32.c times_32_tt.c lucida_32.c lucida_32_tt.c
 GENFONTOBJS = $(GENFONTSRCS:.c=.o)
 
 all: draw
--- draw.c
+++ draw.c
@@ -5,7 +5,7 @@
  * Newline will result in scrolling screen.
  *
  * Sep 2022 Greg Haerr <greg@censoft.com>
- * Aug 2025 Greg Haerr rewritten
+ * Aug 2025 Greg Haerr rewritten, supports bitmap and TTF antialiased fonts
  */
 
 #include <stdint.h>
@@ -13,76 +13,12 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include "draw.h"
 #include <SDL2/SDL.h>
-#include "font.h"
-
-/* supported internal framebuffer pixel formats */
-#define MWPF_DEFAULT       MWPF_TRUECOLORARGB
-#define MWPF_TRUECOLORARGB 0    /* 32bpp, memory byte order B, G, R, A */
-#define MWPF_TRUECOLORABGR 1    /* 32bpp, memory byte order R, G, B, A */
 
 #define MIN(a,b)      ((a) < (b) ? (a) : (b))
 #define MAX(a,b)      ((a) > (b) ? (a) : (b))
-
-typedef uint32_t Pixel;     /* internal pixel format (ARGB or ABGR) */
-typedef uint8_t Alpha;      /* size of alpha channel components */
-
-typedef struct point {
-    int x, y;
-} Point;
-
-typedef struct rect {
-    int x, y;
-    int w, h;
-} Rect;
-
-struct palentry {           /* palette entry */
-    unsigned char r, g, b, a;
-};
-
-typedef struct drawable {
-    int pixtype;            /* pixel format */
-    int bpp;                /* bits per pixel */
-    int bytespp;            /* bytes per pixel */
-    int width;              /* width in pixels */
-    int height;             /* height in pixels */
-    int pitch;              /* stride in bytes, offset to next pixel row */
-    Pixel color;            /* rgb color for glyph blit mode */
-    void *window;           /* opaque pointer for associated window */
-    int32_t size;           /* total size in bytes */
-    uint8_t *pixels;        /* pixel data, normally points to data[] below */
-    Pixel data[];           /* drawable memory allocated in single malloc */
-} Drawable, Texture;
-
-struct console {
-    /* configurable parameters */
-    int cols;               /* # text columns */
-    int lines;              /* # text rows */
-    int char_width;         /* glyph width in pixels */
-    int char_height;        /* glyph height in pixels */
-    Font *font;             /* associated font */
-
-    int curx;               /* cursor x position */
-    int cury;               /* cursor y position */
-    int lastx;
-    int lasty;
-    Rect update;            /* console update region in cols/lines coordinates */
-    Drawable *dp;            //FIXME for testing only
-    uint16_t text_ram[];    /* adaptor RAM (= cols * lines * 2) in single malloc */
-};
-
-/*
- * Conversion from 8-bit RGB components to pixel value
- */
-/* create 32 bit 8/8/8/8 format pixel (0xAARRGGBB) from RGB triplet*/
-#define RGB2PIXELARGB(r,g,b)    \
-    (0xFF000000UL | ((r) << 16) | ((g) << 8) | (b))
-
-/* create 32 bit 8/8/8/8 format pixel (0xAABBGGRR) from RGB triplet*/
-#define RGB2PIXELABGR(r,g,b)    \
-    (0xFF000000UL | ((b) << 16) | ((g) << 8) | (r))
-
-#define RGBDEF(r,g,b)   {r, g, b, 0}
+#define RGBDEF(r,g,b) {r, g, b, 0}
 
 /* palette tables used for console attribute color conversions */
 #if 1
@@ -764,10 +700,10 @@ static Font *fonts[] = {
 #ifndef NOMAIN
     &font_cour_32,
     &font_cour_32_tt,
-    &font_times_32,
-    &font_times_32_tt,
-    &font_lucida_32,
-    &font_lucida_32_tt,
+    //&font_times_32,
+    //&font_times_32_tt,
+    //&font_lucida_32,
+    //&font_lucida_32_tt,
 #endif
 };
 
